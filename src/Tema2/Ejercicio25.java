148b91833ea0f0920ea3ac51063873ad56860006
teoooo4 <teodin4@alu.edu.gva.es>
2025-10-20 10:03:58 +0200

    ejercicios

---
+++ src/Tema2/Ejercicio25.java
@@ -0,0 +1,31 @@
+package Tema2;
+
+import java.util.Scanner;
+
+public class Ejercicio25 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int numero;
+        int factorial = 1;
+
+        System.out.println("Introduce el número entero: ");
+        numero = sc.nextInt();
+
+        if (numero < 0) {
+            System.out.println("El factorial no está definido para números negativos");
+
+        } else {
+            for (int i = 1; i <= numero; i++) {
+                factorial = factorial * i;
+            }
+
+            System.out.println("El facotrial de " + numero + " es: " + factorial);
+
+        }
+
+        sc.close();
+
+    }
+}
