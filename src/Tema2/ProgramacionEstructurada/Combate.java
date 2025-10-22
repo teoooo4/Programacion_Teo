de445f0ec2d5148bfbe046d8e27da575c5b9ca26
teoooo4 <teodin4@alu.edu.gva.es>
2025-10-22 12:29:19 +0200

    new

--- src/Tema2/ProgramacionEstructurada/Combate.java
+++ src/Tema2/ProgramacionEstructurada/Combate.java
@@ -1,4 +1,11 @@
+import java.util.Scanner;
+
 package Tema2.ProgramacionEstructurada;
 
 public class Combate {
+    public static void main(String[] args) {
+
+        System.out.println("¡Bienvenido al programa Combate!");
+
+    }
 }
