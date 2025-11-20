b06f3d4f7b66dc8679dbf23f58e263984562b6ec
teoooo4 <teodin4@alu.edu.gva.es>
2025-11-20 12:29:11 +0100

    new

---
+++ src/Tema3/Arrays/Prueba.java
@@ -0,0 +1,24 @@
+package Tema3.Arrays;
+
+public class Prueba {
+
+    /*
+
+    int[] grades;
+    grades = new int[](0,0,0,0,0);
+
+    int grade = grades[2];
+    System.out.println(grades[2]);
+
+    grades[2] = 70;
+
+    */
+
+
+
+
+
+
+
+
+}
--- src/Tema3/ProgramacionModular1/Ejercicio7.java
+++ src/Tema3/ProgramacionModular1/Ejercicio7.java
@@ -3,17 +3,39 @@ package Tema3.ProgramacionModular1;
 import java.util.Scanner;
 
 public class Ejercicio7 {
-    public static int
-
-
-
-
-
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int numero;
 
-        System.out.println("Escribe");
+        System.out.println("Escribe números enteros (0 para salir):");
 
+        do {
+            numero = sc.nextInt();
+
+            if (numero > 0) {
+                if (esPrimo(numero)) {
+                    System.out.println(numero + " es primo.");
+                } else {
+                    System.out.println(numero + " no es primo.");
+                }
+            }
+
+        } while (numero != 0);
+
+        System.out.println("Fin.");
+        sc.close();
+    }
+
+    public static boolean esPrimo(int n) {
+        if (n < 2) return false;
+
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
